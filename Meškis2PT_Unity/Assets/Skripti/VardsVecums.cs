deed7c63ee2f7213557238825fae697b032d4cbc
Marks Meškis <meshkis.mark@gmail.com>
2022-02-23 14:07:11 +0200

    Input field un pogas izveidosana

---
+++ Meškis2PT_Unity/Assets/Skripti/VardsVecums.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardsVecums : MonoBehaviour {
+
+	public string teksts;
+	public string teksts2;
+	public GameObject ievadesLauks;
+	public GameObject ievadesLauks2;
+	public GameObject tekstaAttelosana;
+
+	public void uzglabaTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		teksts2 = ievadesLauks2.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text=teksts+" ir "+teksts2+" gadus vecs!";
+	}
+
+}
